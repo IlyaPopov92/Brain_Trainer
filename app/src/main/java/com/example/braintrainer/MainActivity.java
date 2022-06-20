88fde0ab28d06c2bd95aac7d8a256802526d3c06
IlyaPopov92 <VfhbzK.,k.1991>
2022-06-20 14:29:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/braintrainer/MainActivity.java
@@ -0,0 +1,145 @@
+package com.example.braintrainer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewOpinion0;
+    private TextView textViewOpinion1;
+    private TextView textViewOpinion2;
+    private TextView textViewOpinion3;
+    private ArrayList<TextView> options = new ArrayList<>();
+    private TextView textViewTimer;
+    private TextView textViewScore;
+    private TextView textViewQuestion;
+
+    private String question;
+    private int rightAnswer;
+    private int rightAnswerPosition;
+    private boolean isPositive;
+    private int min = 5;
+    private int max = 30;
+
+    private int countOfQuestion = 0;
+    private int countOfRightAnswer = 0;
+
+    private boolean gameOver = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewOpinion0 = findViewById(R.id.textViewOpinion0);
+        textViewOpinion1 = findViewById(R.id.textViewOpinion1);
+        textViewOpinion2 = findViewById(R.id.textViewOpinion2);
+        textViewOpinion3 = findViewById(R.id.textViewOpinion3);
+        textViewTimer = findViewById(R.id.textViewTimer);
+        textViewScore = findViewById(R.id.textViewScore);
+        textViewQuestion = findViewById(R.id.textViewQuestion);
+
+        options.add(textViewOpinion0);
+        options.add(textViewOpinion1);
+        options.add(textViewOpinion2);
+        options.add(textViewOpinion3);
+
+        playNext();
+
+        CountDownTimer timer = new CountDownTimer(20000, 100) {
+            @Override
+            public void onTick(long l) {
+                textViewTimer.setText(getTime(l));
+                if (l < 10000) {
+                    textViewTimer.setTextColor(getResources().getColor(android.R.color.holo_red_light,getTheme()));
+                }
+            }
+
+            @Override
+            public void onFinish() {
+//                gameOver = true;
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                int max = preferences.getInt("max", 0);
+                if (countOfRightAnswer >= max) {
+                    preferences.edit().putInt("max", countOfRightAnswer).apply();
+                }
+                Intent intent = new Intent(MainActivity.this, ScoreActivity.class);
+                intent.putExtra("result", countOfRightAnswer);
+                startActivity(intent);
+            }
+        };
+        timer.start();
+
+    }
+
+    private void playNext() {
+        generateQuestion();
+        for (int i = 0; i < options.size(); i++) {
+            if (i == rightAnswerPosition) {
+                options.get(i).setText(Integer.toString(rightAnswer));
+            } else {
+                options.get(i).setText(Integer.toString(generateWrongAnswer()));
+            }
+        }
+        String score = String.format("%s | %s", countOfQuestion, countOfRightAnswer);
+        textViewScore.setText(score);
+    }
+
+    private void generateQuestion() {
+        int a = (int) (Math.random() * (max - min + 1) + min);
+        int b = (int) (Math.random() * (max - min + 1) + min);
+        int mark = (int) (Math.random() * 2);
+        isPositive = mark == 1;
+        if (isPositive) {
+            rightAnswer = a + b;
+            question = String.format("%s + %s", a, b);
+        } else {
+            rightAnswer = a - b;
+            question = String.format("%s - %s", a, b);
+        }
+        textViewQuestion.setText(question);
+        rightAnswerPosition = (int) (Math.random() * 4);
+    }
+
+    private int generateWrongAnswer() {
+        int result;
+        do {
+            result = (int) (Math.random() * max * 2 + 1) - (max - min);
+        } while (result == rightAnswer);
+        return result;
+    }
+
+    private String getTime(long l) {
+        int seconds = (int) (l / 1000);
+        int minutes = seconds / 60;
+        seconds = seconds % 60;
+        String result = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+        return result;
+    }
+
+    public void onClickAnswer(View view) {
+        if (!gameOver) {
+            TextView textView = (TextView) view;
+            String answer = textView.getText().toString();
+            int answerChoosen = Integer.parseInt(answer);
+            if (answerChoosen == rightAnswer) {
+                countOfRightAnswer++;
+                Toast.makeText(this, "Верно", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Неверно", Toast.LENGTH_SHORT).show();
+            }
+            countOfQuestion++;
+            playNext();
+        }
+    }
+}
